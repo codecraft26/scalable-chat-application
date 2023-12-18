ee5855fc26da5f6f7bcb1009ff114bdccb713fc0
codecraft26 <gaman0221@gmial.com>
2023-12-18 22:56:30 +0530

    Add server files and dependencies

---
+++ apps/server/src/index.ts
@@ -0,0 +1,13 @@
+import http from 'http'
+
+async function init() {
+
+    const httpServer=http.createServer();
+    const PORT=process.env.PORT ? process.env.PORT:8000
+    httpServer.listen(PORT,()=>{
+        console.log(`Server is listening on port ${PORT}`)
+    })
+    
+}
+
+init()
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,31 @@
+import { Server } from 'socket.io'
+
+
+class SocketService{
+        private _io:Server
+
+    constructor(){
+
+        console.log('SocketService constructor')
+
+        this._io=new Server()
+
+    }
+
+
+    get io(){
+        return this._io
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+export default SocketService;
